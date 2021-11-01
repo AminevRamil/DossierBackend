41d9339c9ad68fb8f1df7c7c4f0e415bfd91a5d0
AminevRamil <Optimus1379@yandex.ru>
2021-11-02 00:47:42 +0400

    Initial commit

---
+++ src/test/java/com/starbun/dossierbackend/DossierBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbun.dossierbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DossierBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
